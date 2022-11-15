2ed936f0f41a1c2c219bb970f5bca074caf291bd
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-11-15 21:25:22 +0300

    Create CRUD operation class

---
+++ Model/DataWorker.cs
@@ -0,0 +1,72 @@
+﻿using GaspromDiagnostics.Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaspromDiagnostics.Model
+{
+    public static class DataWorker
+    {
+        // Получить все объекты
+        public static List<Object> GetAllObjects()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var result = db.Objects.ToList();
+                return result;
+            }
+        }
+
+        // создать объекты
+        public static void CreateObject(string name, float distance, float angle,
+            float width, float heigth, bool isDefect)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Object newObject = new Object
+                {
+                    Name = name,
+                    Angle = angle,
+                    Width = width,
+                    Heigth = heigth,
+                    IsDefect = isDefect
+                };
+                db.Objects.Add(newObject);
+                db.SaveChanges();
+            }
+        }
+
+        // удаление объекта
+        public static void DeleteObject(Object obj)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Objects.Remove(obj);
+                db.SaveChanges();
+            }
+        }
+
+        // редактирование объекта
+        public static void EditObject(Object oldObject, string newName, float newDistance, 
+            float newAngle, float newWidth, float newHeigth, bool newIsDefect)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Object newObject = db.Objects.First(pos => pos.Id == oldObject.Id);
+                if (newObject != null)
+                {
+                    newObject.Name = newName;
+                    newObject.Distance = newDistance;
+                    newObject.Angle = newAngle;
+                    newObject.Width = newWidth;
+                    newObject.Heigth = newHeigth;
+                    newObject.IsDefect = newIsDefect;
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
