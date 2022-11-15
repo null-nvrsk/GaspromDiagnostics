20fc7261ba44c0d3f595ee59021edea965cf3a2d
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-11-15 16:33:15 +0300

    Add initial database

---
+++ Migrations/20221115132004_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GaspromDiagnostics.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Objects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Distance = table.Column<float>(type: "REAL", nullable: false),
+                    Angle = table.Column<float>(type: "REAL", nullable: false),
+                    Width = table.Column<float>(type: "REAL", nullable: false),
+                    Heigth = table.Column<float>(type: "REAL", nullable: false),
+                    IsDefect = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Objects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Objects");
+        }
+    }
+}
---
+++ Model/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GaspromDiagnostics.Model.Data
+{
+    class ApplicationContext: DbContext
+    {
+        public DbSet<Object> Objects { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=.\\Objects.sqlite;");
+        }
+    }
+}
