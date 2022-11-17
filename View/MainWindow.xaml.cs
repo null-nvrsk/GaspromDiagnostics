a060086138c67ea1e8390b624b88b04b14c8c5ec
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-11-17 11:28:45 +0300

    Add CRUD buttons

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GaspromDiagnostics.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,30 +27,16 @@ namespace GaspromDiagnostics
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new DataManageVM();
 
-            objects = new List<Object>
-            {
-                new Object { Id=1, Name="Коррозия", Distance=2, Angle=7, Width=3, Heigth=2, IsDefect=true },
-                new Object { Id=2, Name="Верт.шов", Distance=5, Angle=7, Width=0, Heigth=4, IsDefect=false },
-                new Object { Id=3, Name="Гор.шов", Distance=5, Angle=1, Width=4, Heigth=0, IsDefect=false }
-            };
-            objectsGrid.ItemsSource = objects;
-            objectList.ItemsSource = objects;
-        }
-
-        private void btnLoad_Click(object sender, RoutedEventArgs e)
-        {
-            //
-        }
-
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnClear_Click(object sender, RoutedEventArgs e)
-        {
-            objects.Clear();
+            //objects = new List<Object>
+            //{
+            //    new Object { Id=1, Name="Коррозия", Distance=2, Angle=7, Width=3, Heigth=2, IsDefect=true },
+            //    new Object { Id=2, Name="Верт.шов", Distance=5, Angle=7, Width=0, Heigth=4, IsDefect=false },
+            //    new Object { Id=3, Name="Гор.шов", Distance=5, Angle=1, Width=4, Heigth=0, IsDefect=false }
+            //};
+            //objectsGrid.ItemsSource = objects;
+            //objectList.ItemsSource = objects;
         }
 
     }
